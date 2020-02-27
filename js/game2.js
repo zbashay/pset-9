7dc87ed0dc6922c49ce95bf0c1c0aa621068482e
zbashay <ronatorino@gmail.com>
2020-02-27 09:06:11 -0600

    The net

--- js/game2.js
+++ js/game2.js
@@ -79,7 +79,7 @@ function collisionDetection() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "coral";
   ctx.fill();
   ctx.closePath();
 }
@@ -109,12 +109,12 @@ function drawBricks() {
 }
 function drawScore() {
   ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "coral";
   ctx.fillText("Score: "+score, 8, 20);
 }
 function drawLives() {
   ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = "coral";
   ctx.fillText("Lives: "+lives, canvas.width-65, 20);
 }
 
